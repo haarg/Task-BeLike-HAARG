db542419b2e8819eb5bec42d9549406f877e1af3
Graham Knop <haarg@haarg.org>
2019-03-08 01:57:37 +0100

    update packaging

---
+++ lib/Task/BeLike/HAARG.pm
@@ -0,0 +1,43 @@
+package Task::BeLike::HAARG;
+use strict;
+use warnings;
+
+our $VERSION = '0.001000';
+$VERSION =~ tr/_//d;
+
+1;
+__END__
+
+=head1 NAME
+
+Task::BeLike::HAARG - Install modules haarg uses
+
+=head1 SYNOPSIS
+
+  cpanm Task::BeLike::HAARG
+
+=head1 DESCRIPTION
+
+This is a L<Task> module that includes most modules used by haarg as
+prerequisites.  This is not intended as an endorsement of the modules, merely
+that they are often needed.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head2 CONTRIBUTORS
+
+None so far.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2019 the Task::BeLike::HAARG L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
+
+=cut
